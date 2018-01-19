eaa78cef5a626b2e58abd3d540014851347fc275
dev85-indglobal <dev85@indglobal-consulting.com>
2018-01-19 12:56:29 +0530

    Initial setup with register api completed

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+date_default_timezone_set('Asia/Kolkata');
+Route::group(['middleware' => 'cors', 'prefix' => '/v1'], function () {
+	  Route::post('authenticate/register', 'AppAuthController@register');
+	  Route::post('authenticate/resend_otp', 'AppAuthController@resend_otp');
+	  Route::post('authenticate/confirm_otp', 'AppAuthController@confirm_otp');
+	  Route::post('authenticate/forget_password', 'AppAuthController@forgotPassword');
+      Route::post('authenticate/confirm_forget_password_otp', 'AppAuthController@confirm_forget_password_otp');
 });
